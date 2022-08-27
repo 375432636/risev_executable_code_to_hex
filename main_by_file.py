88249cee62fb29fc999b1d146c4d1340b9b39b86
John Huang <john.huang@starcomww.net>
2022-08-28 01:23:08 +0800

    添加可切换大小端的命令--low_end

--- main_by_file.py
+++ main_by_file.py
@@ -1,25 +1,41 @@
-import re
+import re,sys
+
+_LOW_END = False # 是否根据进行大小端的反向 
+
+def get_argument():
+    # 判断参数
+    argument = sys.argv
+    print(argument)
+    flag = False
+    if "--low_end" in argument:
+        global _LOW_END
+        _LOW_END = True
 
 # 将代码内容放在executable_code.txt中， 执行python3 main_by_file.py, 结果将会保存在mem.txt
 
-with open("executable_code.txt", "r") as f:
-    text = f.read()
 
 import re
 def tranlate(text):
+    get_argument()
     # 通过正则表达式将hex code提取出来
     hex_code_list = re.findall("([0-9a-fA-F]+):\s+([0-9a-fA-F]+)\s+(\w[^\n]+)",text)
     hex_code_list = [(int(i[0],16),)+i for i in hex_code_list]
     output_list = dict([[i,"00 00 00 00 // %s:"%hex(i)] for i in range(0,max([i[0] for i in hex_code_list]),4)])
     for index,hex_index,hex_code,comment in hex_code_list:
-        hex_code = " ".join([hex_code[j-2:j] for j in range((len(hex_code)),0,-2)])
+        if _LOW_END:
+            hex_code = " ".join([hex_code[j-2:j] for j in range((len(hex_code)),0,-2)])
+        else:
+            hex_code = " ".join([hex_code[j:j+2] for j in range(0,(len(hex_code)),2)])
         output_list[index] = "%s //\t %s: %s"%(hex_code,hex(index),comment) 
     # for index,hex_index, in hex_code_list:
     return "\n".join(output_list.values())
 
-mem = tranlate(text)
 
-with open("mem.txt", "w") as f:
-    f.write(mem)
+if __name__ == "__main__":
+    with open("executable_code.txt", "r") as f:
+        text = f.read()
+    mem = tranlate(text)
+    with open("mem.txt", "w") as f:
+        f.write(mem)
 
-print("success!! pls check the file named mem.txt")
+    print("success!! pls check the file named mem.txt")
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 # 功能
 
-将Ripes的executable code转换为hex ram
+* 将Ripes的executable code转换为hex ram
+
+* 通过命令`--low_end`可以输出小端内容的ram(一行内按word反向), 默认为大端输出(不反向)
 
 1. 输入内容
 
@@ -18,6 +20,22 @@
 
 2. 输出内容
 
+* 大端输出
+
+`python main.py`
+
+```text
+00 60 04 13 //   0x0: addi x8 x0 6
+00 90 04 93 //   0x4: addi x9 x0 9
+00 94 05 33 //   0x8: add x10 x8 x9
+00 60 04 13 //   0xc: addi x8 x0 6
+00 90 04 93 //   0x10: addi x9 x0 9
+00 94 05 33 //   0x14: add x10 x8 x9
+```
+* 小端输出
+
+`python main.py --low_end`
+
 ```text
 13 04 60 00 //   0x0: addi x8 x0 6
 93 04 90 00 //   0x4: addi x9 x0 9
@@ -26,6 +44,7 @@
 93 04 90 00 //   0x10: addi x9 x0 9
 33 05 94 00 //   0x14: add x10 x8 x9
 ```
+
 
 # 使用方法
 
@@ -46,6 +65,7 @@
 ## 20220828
 
 * 修复一部分C代码转换的问题, 给不存在的代码填0x00
+* 添加可切换大小端的命令`--low_end`
 
 ## 20220826
 
